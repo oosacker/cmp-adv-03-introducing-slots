fafbe1f976c2b98b238ae5dee9864e323f00edc6
Natsuki Hasegawa <oosacker@gmail.com>
2022-01-03 18:06:02 +1300

    slots

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another="another 4"></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ['this is a string', 'number2']
+    }
+  }
+}
+</script>
