61655ae858df7e27bade6147db472dc6de02a95d
Natsuki Hasegawa <oosacker@gmail.com>
2022-01-06 11:37:15 +1300

    dynamic components

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,18 @@
       :role="activeUser.role"
     ></user-info>
 
+    <!-- Slots -->
     <course-goals #default="slotProps">
         <h2>{{ slotProps.item }}</h2>
         <p>{{ slotProps['another'] }}</p>
     </course-goals>
+
+    <!-- Dynamic Components -->
+    <button @click="setSelectedComp('active-goals')">Active Goals</button>
+    <button @click="setSelectedComp('manage-goals')">Manage Goals</button>
+    <!-- <active-goals></active-goals>
+    <manage-goals></manage-goals> -->
+    <component :is="activeComp"></component>
 
   </div>
 </template>
@@ -23,13 +31,17 @@ import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
-    CourseGoals
+    CourseGoals,
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
@@ -38,8 +50,14 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      activeComp: ''
     };
   },
+  methods: {
+    setSelectedComp(comp) {
+      this.activeComp = comp
+    }
+  }
 };
 </script>
 
